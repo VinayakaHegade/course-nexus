77ea59b1a6fc0231ff3a5f79c9941e681b5ff190
Vinayak <myselfvinayakhegde@gmail.com>
2023-11-09 02:38:06 +0530

    feat: add exit button to navbar

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,11 +1,29 @@
-"use client"
+"use client";
 
 import { UserButton } from "@clerk/nextjs";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Button } from "./ui/button";
 
 const NavbarRoutes = () => {
-    return ( <div className="flex gap-x-2 ml-auto">
-        <UserButton/>
-    </div> );
-}
+  const pathname = usePathname();
+
+  const isPlayerPage = pathname?.includes("/chapter");
+
+  return (
+    <div className="flex gap-x-2 ml-auto">
+      {isPlayerPage ? (
+        <Link href="/">
+          <Button size="sm" variant="ghost">
+            <LogOut className="h-4 w-4 mr-2" />
+            Exit
+          </Button>
+        </Link>
+      ) : null}
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+};
 
 export default NavbarRoutes;
