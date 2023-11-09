9f9ef2d5d5b348e90b39d75b686ac7e7017d4a17
Vinayak <myselfvinayakhegde@gmail.com>
2023-11-09 15:27:48 +0530

    feat: add category filter tab

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,5 +1,19 @@
-const SearchPage = () => {
-  return <div>This is search Page</div>;
+import { db } from "@/lib/db";
+import Categories from "./_components/categories";
+
+const SearchPage = async () => {
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+
+  return (
+    <section className="p-6">
+      <Categories items={categories} />
+      <p>This is search page</p>
+    </section>
+  );
 };
 
 export default SearchPage;
