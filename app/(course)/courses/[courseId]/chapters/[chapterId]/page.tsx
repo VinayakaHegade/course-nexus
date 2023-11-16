3b22d0cdf1b058dab4e801807815d6fb3b1f6bc4
Vinayak <myselfvinayakhegde@gmail.com>
2023-11-17 01:54:17 +0530

    feat: intialize chapter page

---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -0,0 +1,43 @@
+import { getChapter } from "@/actions/get-chapter";
+import { Banner } from "@/components/banner";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const ChapterIdPage = async ({
+  params,
+}: {
+  params: { courseId: string; chpaterId: string };
+}) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const { chapter, course, attachments, nextChapter, userProgress, purchase } =
+    await getChapter({
+      userId,
+      chapterId: params.chpaterId,
+      courseId: params.courseId,
+    });
+
+  if (!chapter || !course) {
+    return redirect("/");
+  }
+
+  const isLocked = !chapter.isFree && !purchase;
+  const completeOnEnd = !!purchase && !userProgress?.isCompleted;
+
+  return (
+    <section>
+      {userProgress?.isCompleted && (
+        <Banner variant="success" label="You already completed this chapter" />
+      )}
+      {isLocked && (
+        <Banner variant="warning" label="You already completed this chapter." />
+      )}
+    </section>
+  );
+};
+
+export default ChapterIdPage;
